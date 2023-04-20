3c9b0a1563ec8cb1dced15b65215a1b736d46d06
Carlos Prosdoskimi <henriqueprosdoskimi@hotmail.com>
2023-04-20 13:50:31 -0300

    removendo arquivos e adicionando styled components e estilização global

---
+++ src/pages/Login/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container, Form } from "./styles";
+
+export const Login = () => {
+  return (
+    <Container>
+      <Form></Form>
+    </Container>
+  );
+};
---
+++ src/styles/global.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  width: 100vw;
+  height: 100vh;
+  background-color: #f0f2f5;
+  font-family:Poppins;
+
+}
+
+`;
+
+/*box-sizing: border-box serve para que possamos nao se preocupar com as medidas, ou seja, 
+height e width sempre serão os mesmo independente do padding ou margin. Se você tiver um componente com 100px
+de largura (width), e ele receber 10px de padding, ele ira diminuir os elementos filhos para poder manter esses 10px totais da largura
+*/
